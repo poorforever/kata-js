d090e73b34443aed4e54f20dc4a142834f7bda98
Anaël <anaelmanzambi@gmail.com>
2025-12-13 18:42:12 +0100

    Doc et consigne de l'exercice

--- morse-letter-keyrow-finder.js
+++ morse-letter-keyrow-finder.js
@@ -1,3 +1,25 @@
+/**
+  Le but de cet exercice est d'écrire une fonction prenant en entrée une chaine de caractère correspondant au code morse
+  d'une lettre de l'alphabet, et renvoyer un chiffre correspondant à l'indice de l'une des trois rangées de lettre
+  sur un clavier (AZERTY).
+
+  Les différents rangées sont les suivants:
+  - AZERTYUIOP => 1
+  - QSDFGHJKLM => 2
+  - WXCVBN => 3
+
+  Exemple: `_.__` correspond à la lettre Y en morse, lettre qui se trouve sur la rangée 1 d'un clavier AZERTY.
+  Donc find(`_.__`) = 1
+
+  Ne pas hésister à créer de nouvelles méthodes, ainsi que des constantes.
+
+  Bonus: Gérer le cas d'un clavier QWERTY. Ne pas hésiter à modifier la signature de la méthode.
+
+ * @param morseLetter
+ * @param isAzerty
+ * @return number keyrow
+ */
+
 export function find(morseLetter, isAzerty) {
     const letter = fromMorseLetter(morseLetter);
     const rows = isAzerty ? azerty_keyrows : qwerty_rows
