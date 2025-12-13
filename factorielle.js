4c676ee59bc49e157b1b1eb44e8c695c50dd6cd1
Anaël <anaelmanzambi@gmail.com>
2025-12-13 01:47:11 +0100

    Cleaning solution

--- factorielle.js
+++ factorielle.js
@@ -1,7 +1,1 @@
-export function factorial (n) {
-    if(n < 0)
-        throw new Error('n must be a positive integer')
-    if(n === 0)
-        return 1
-    return n * factorial(n-1)
-}
+export function factorial (n) {}
